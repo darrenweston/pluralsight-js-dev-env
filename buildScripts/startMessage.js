61e54bf560396b74c8382b3ea1c34340c41faa00
dweston <daz.weston@gmail.com>
2017-03-02 12:16:17 +0000

    add babel transpiling support

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
+import chalk from 'chalk';  // we can use ES6 features because we are
+                            // transpiling with babel
 
 console.log(chalk.green('Starting app in dev mode...'));
