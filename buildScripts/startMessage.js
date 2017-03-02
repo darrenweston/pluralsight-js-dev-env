9899830e75403e098a0a8fc1a6f6ccf7cd173e90
dweston <daz.weston@gmail.com>
2017-03-02 14:20:19 +0000

    add eslint support

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,4 +2,4 @@
 import chalk from 'chalk';  // we can use ES6 features because we are
                             // transpiling with babel
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css'; // for webpack to bundle for us
 
-import numeral from 'numeral';
+// import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
+// const courseValue = numeral(1000).format('$0,0.00');
 // debugger  // uncomment this to set breakpoint
-console.log(`I would not pay ${courseValue} for this course!`);
+//console.log(`I would not pay ${courseValue} for this course!`);
