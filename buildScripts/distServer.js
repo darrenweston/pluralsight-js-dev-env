667820114b2410ddd8ca5991b53bc4d1e1932d79
dweston <daz.weston@gmail.com>
2017-03-05 13:47:28 +0000

    add build files

---
+++ buildScripts/distServer.js
@@ -0,0 +1,36 @@
+// This is NOT for actual production use. This is just for hosting the
+// minified production build for local debugging purposes.
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res) {
+    // Hard coding for simplicity. Pretend this hits a real database
+    res.json([
+        {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+        {"id": 2, "firstName": "Alice", "lastName": "Wonderland", "email": "aw@yahoo.com"},
+        {"id": 3, "firstName": "Albert", "lastName": "Einstein", "email": "alby@hotmail.com"}
+    ]);
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
